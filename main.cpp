718d88e766e97475f5b378e4b7d38fa800db47f0
Franz <franzbischoff@gmail.com>
2015-10-12 17:12:34 +0100

    Duplicated keys and comments

--- main.cpp
+++ main.cpp
@@ -30,40 +30,6 @@
  *
  */
 
-/**
-    Exemplo de Input 1
-
-    4 3
-    manuel 10 50 20
-    joao 20 30 20
-    rita 20 100 0
-    ana 0 0 10
-
-    Exemplo de Output 1
-
-    rita 120
-    manuel 80
-    joao 70
-    ana 10
-
-    Exemplo de Input 2
-
-    5 2
-    pedro 10 20
-    joao 20 10
-    miguel 0 30
-    joana 100 100
-    cristina 5 5
-
-    Exemplo de Output 2
-
-    joana 200
-    joao 30
-    miguel 30
-    pedro 30
-    cristina 10
- */
-
 #include <cstdlib>
 #include <iostream>
 #include <cstring>
@@ -86,9 +52,28 @@ int main(int argc, char** argv)
     uint32_t score;
     char arg;
     bool interactive = false;
+    bool dups = false;
     bool(*fn_pt)(pair<uint32_t, string>, pair<uint32_t, string>) = fncomp;
 
+    /**
+     * Create a set (or multiset for duplicated keys) of a paired value, score - name.
+     * This allow us to use a personalized function to define how the data is sorted
+     *
+     * Why use set? Set uses a binary three implementation. This method is fast for inserting
+     * ordered elements, but is not optimized for retrieving random elements.
+     *
+     * Since we won't use random access to output the results, this is the best approach.
+     *
+     * We could think in using map or multimap for this purpose as we need a "key" (the score)
+     * and a "value" the name. It indeed also uses a binary three implementation and sorts the
+     * elements by "key", but does not allow to set a proper rule for sorting the values.
+     * Most implementations keep the values in the inserted order. So to achieve the purpose
+     * of this exercise, we would have to sort the grouped (by key) values during the output time.
+     *
+     */
+
     set < pair<uint32_t, string>, bool(*)(pair<uint32_t, string>, pair<uint32_t, string>) > scoreset(fn_pt);
+    multiset < pair<uint32_t, string>, bool(*)(pair<uint32_t, string>, pair<uint32_t, string>) > scoremultiset(fn_pt);
 
 
     for (i = 1; i < argc; i++)
@@ -105,6 +90,9 @@ int main(int argc, char** argv)
                 {
                 case 'i':
                     interactive = true;
+                    break;
+                case 'd':
+                    dups = true;
                     break;
                 case 'h':
                     print_help();
@@ -137,23 +125,53 @@ int main(int argc, char** argv)
             score += atoi(t.c_str());
         }
 
-        scoreset.insert(pair<uint32_t, string>(score, name));
+        // use multiset to allow duplicated keys
+        if (dups)
+        {
+            scoremultiset.insert(pair<uint32_t, string>(score, name));
+        }
+        else
+        {
+            scoreset.insert(pair<uint32_t, string>(score, name));
+        }
+
     }
 
-    for (set< pair<uint32_t, string> >::reverse_iterator it = scoreset.rbegin(); it != scoreset.rend(); ++it)
-        cout << (*it).second << " " << (*it).first << '\n';
+    if (dups)
+    {
+        for (multiset< pair<uint32_t, string> >::reverse_iterator it = scoremultiset.rbegin(); it != scoremultiset.rend(); ++it)
+            cout << (*it).second << " " << (*it).first << '\n';
+    }
+    else
+    {
+        for (set< pair<uint32_t, string> >::reverse_iterator it = scoreset.rbegin(); it != scoreset.rend(); ++it)
+            cout << (*it).second << " " << (*it).first << '\n';
+    }
 
     return 0;
 }
 
+/**
+ * This function defines how data is sorted.
+ *
+ * First we check if the score (first) is equal. If so, we order the name (second) by alphabetical order.
+ *
+ * As the final output is in inverse order (first the larger score) we need to sort the name by reverse order.
+ *
+ * @param lhs
+ * @param rhs
+ * @return
+ */
 bool fncomp(pair<uint32_t, string> lhs, pair<uint32_t, string> rhs)
 {
     if (lhs.first == rhs.first)
     {
+        // sort in reverse alphabetical order
         return rhs.second.compare(lhs.second) < 0;
     }
     else
     {
+        // is the left smaller than the right?
         return lhs.first < rhs.first;
     }
 }
@@ -168,13 +186,14 @@ void print_help()
     cout << "\tINPUT:" << endl << "\t\tThis program uses the stdin. Type 'program < input.txt' for file input or use the -i argument for interactive mode." << endl << endl;
 
     cout << "\tFORMAT:" << endl << "\t\tThe first line holds an integer 'N' with the number of competitors and an integer 'P' with the number of problems. These numbers must be 1 <= N <= 50,000 and 1 <= P <= 10." << endl;
-    cout << "\t\tThe following 'N' lines are composed by an unique string with the first name of the competitor, followed by 'P' integers with the score of each test from 0 to 100." << endl << endl;
+    cout << "\t\tThe following 'N' lines are composed of an unique string with the first name of the competitor, followed by 'P' integers with the score of each test from 0 to 100." << endl << endl;
 
     cout << "\tOUTPUT:" << endl << "\t\tThe output is composed of N lines. The first column is the name of the competitor and the second column is the score." << endl << endl;
 
     cout << "OPTIONS" << endl << endl;
     cout << "\t-h This help." << endl << endl;
     cout << "\t-i Interactive mode." << endl << endl;
+    cout << "\t-d Allow duplicated names." << endl << endl;
 
     cout << "AUTHOR" << endl << endl;
     cout << "\tFrancisco Bischoff." << endl << endl;
